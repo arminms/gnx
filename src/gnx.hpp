2e09411a32d7fccb12d6ea01aea01cbffb1b3d00
Armin Sobhani <arminms@gmail.com>
2026-04-17 19:14:38 -0400

    feat: add gnx_options struct for configuration options in gnx CLI

---
+++ src/gnx.hpp
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2026 Armin Sobhani
+//
+#pragma once
+
+#include <vector>
+#include <string>
+#include <memory>
+
+#include <CLI/CLI.hpp>
+#include <CLI/Timer.hpp>
+#include <fmt/core.h>
+
+#include "ansi.hpp"
+
+struct gnx_options
+{   bool time_it{false};
+};
