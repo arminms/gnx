9533ad993e1a94f7f6e65302d7ecc4e2485fe40b
Armin Sobhani <arminms@gmail.com>
2026-04-20 18:54:03 -0400

    feat: add printerr function for error message handling and return_code in gnx.hpp

--- src/gnx.hpp
+++ src/gnx.hpp
@@ -6,6 +6,9 @@
 #include <vector>
 #include <string>
 #include <memory>
+#include <stdexcept>
+#include <cstdio>
+#include <filesystem>
 
 #include <CLI/CLI.hpp>
 #include <CLI/Timer.hpp>
@@ -13,6 +16,17 @@
 
 #include "ansi.hpp"
 
+/// @brief Prints an error message to stderr
+/// @tparam ...Args format string arguments
+/// @param fmt format string
+/// @param ...args format string arguments 
+template <typename... Args>
+void printerr(fmt::format_string<Args...> fmt, Args&&... args)
+{   fmt::print(stderr, fmt, std::forward<Args>(args)...);
+}
+
+/// @brief A struct to hold global options and state for the gnx command-line tool
 struct gnx_options
 {   bool time_it{false};
+    int return_code{0};
 };
