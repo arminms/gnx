b8f956becd06351fb6d781b0e022f999860d3622
Armin Sobhani <arminms@gmail.com>
2026-05-14 18:59:15 -0400

    feat: enhance describe() function to provide output for fastq files, including read length and encoding information

--- include/gnx/utility/describe.hpp
+++ include/gnx/utility/describe.hpp
@@ -28,53 +28,142 @@ inline void describe(std::string_view filename)
         return;
     }
 
-    // bool is_fastq = !stream.quality().empty();
+    // human-readable size formatter
+    auto hrs = [](std::size_t size) -> std::string
+    {   int suffix_index{0};
+        double human_size = static_cast<double>(size);
+        while (human_size >= 1000 && suffix_index < 4)
+        {   human_size /= 1000;
+            ++suffix_index;
+        }
+        return fmt::format("{:.1f} {}bp", human_size, " KMGT"[suffix_index]);
+    };
 
-    std::size_t count{0}, total_length{0};
-    fmt::memory_buffer buf;
-    fmt::format_to(std::back_inserter(buf)
-    ,   "▼ {}{}{}\n"
-    ,   gnx::ansi::escape[fg::bright_green]
-    ,   std::filesystem::path(filename).stem().string()
-    ,   gnx::ansi::escape[fg::reset]
-    );
-    for (; it != end_it; ++it, ++count)
-    {   fmt::format_to(std::back_inserter(buf)
-        ,   "├── ▼ {}{}{}\n"
-        ,   gnx::ansi::escape[fg::bright_magenta]
-        ,   it->id()
+    if (stream.quality().empty())
+    {   std::size_t count{0}, total_length{0};
+        fmt::memory_buffer buf;
+        fmt::format_to(std::back_inserter(buf)
+        ,   "▼ {}{}{}\n"
+        ,   gnx::ansi::escape[fg::bright_green]
+        ,   std::filesystem::path(filename).stem().string()
         ,   gnx::ansi::escape[fg::reset]
         );
-        if (it->description().size() > 0)
+        for (; it != end_it; ++it, ++count)
+        {   fmt::format_to(std::back_inserter(buf)
+            ,   "├── ▼ {}{}{}\n"
+            ,   gnx::ansi::escape[fg::bright_magenta]
+            ,   it->id()
+            ,   gnx::ansi::escape[fg::reset]
+            );
+            if (it->description().size() > 0)
+                fmt::format_to
+                (   std::back_inserter(buf)
+                ,   "│   ├── {}{}{}\n"
+                ,   gnx::ansi::escape[fg::bright_cyan]
+                +   gnx::ansi::escape[style::italic]
+                ,   it->description()
+                ,   gnx::ansi::escape[style::reset]
+                );
             fmt::format_to
             (   std::back_inserter(buf)
-            ,   "│   ├── {}{}{}\n"
-            ,   gnx::ansi::escape[fg::bright_cyan]
-            +   gnx::ansi::escape[style::italic]
-            ,   it->description()
-            ,   gnx::ansi::escape[style::reset]
+            ,   "│   └── {}{} ({}){}\n"
+            ,   gnx::ansi::escape[fg::bright_yellow]
+            ,   hrs(it->sequence().size())
+            ,   it->sequence().size()
+            ,   gnx::ansi::escape[fg::reset]
             );
+            total_length += it->sequence().size();
+        }
         fmt::format_to
         (   std::back_inserter(buf)
-        ,   "│   └── {}{}{} bp\n"
+        ,   "└── {}{}{} {}, {}{} ({}){}\n"
         ,   gnx::ansi::escape[fg::bright_yellow]
-        ,   it->sequence().size()
+        ,   count
+        ,   gnx::ansi::escape[fg::reset]
+        ,   (count > 1) ? "sequences" : "sequence"
+        ,   gnx::ansi::escape[fg::bright_yellow]
+        ,   hrs(total_length)
+        ,   total_length
         ,   gnx::ansi::escape[fg::reset]
         );
-        total_length += it->sequence().size();
+        std::cout.write(buf.data(), buf.size());
     }
-    fmt::format_to
-    (   std::back_inserter(buf)
-    ,   "└── {}{}{} {}, {}{}{} bp\n"
-    ,   gnx::ansi::escape[fg::bright_yellow]
-    ,   count
-    ,   gnx::ansi::escape[fg::reset]
-    ,   (count > 1) ? "sequences" : "sequence"
-    ,   gnx::ansi::escape[fg::bright_yellow]
-    ,   total_length
-    ,   gnx::ansi::escape[fg::reset]
-    );
-    std::cout.write(buf.data(), buf.size());
+    else  // fastq format containing reads
+    {   std::size_t count{0}, total_length{0}, min_length{0}, max_length{0};
+        bool phred33{false};
+        fmt::memory_buffer buf;
+        fmt::format_to(std::back_inserter(buf)
+        ,   "▼ {}{}{}\n"
+        ,   gnx::ansi::escape[fg::bright_green]
+        ,   std::filesystem::path(filename).stem().string()
+        ,   gnx::ansi::escape[fg::reset]
+        );
+        for (; it != end_it; ++it, ++count)
+        {   min_length
+            =   (count == 0)
+            ?   it->sequence().size()
+            :   std::min(min_length, it->sequence().size());
+            max_length
+            =   (count == 0)
+            ?   it->sequence().size()
+            :   std::max(max_length, it->sequence().size());
+            if (phred33 == false && count < 10000)
+            {   for (char c : stream.quality())
+                {   if (c < 58 || c > 1)
+                    {   phred33 = true;
+                        break;
+                    }
+                }
+            }
+            total_length += it->sequence().size();
+        }
+        fmt::format_to
+        (   std::back_inserter(buf)
+        ,   "├── Encoding: {}{}{}\n"
+        ,   gnx::ansi::escape[fg::bright_cyan]
+        +   gnx::ansi::escape[style::italic]
+        ,   phred33
+            ?   min_length > 1000
+                ?   "PacBio / Nanopore"
+                :   "Sanger / Illumina 1.8+"
+            :   "Illumina 1.3 - 1.7"
+        ,   gnx::ansi::escape[style::reset]
+        );
+        fmt::format_to
+        (   std::back_inserter(buf)
+        ,   "├── Total reads: {}{}{}\n"
+        ,   gnx::ansi::escape[fg::bright_yellow]
+        ,   count
+        ,   gnx::ansi::escape[fg::reset]
+        );
+        fmt::format_to
+        (   std::back_inserter(buf)
+        ,   "├── Total bases: {}{} ({}){}\n"
+        ,   gnx::ansi::escape[fg::bright_yellow]
+        ,   hrs(total_length)
+        ,   total_length
+        ,   gnx::ansi::escape[fg::reset]
+        );
+        if (max_length > min_length)
+            fmt::format_to
+            (   std::back_inserter(buf)
+            ,   "└── Read length: {}{}-{}{}\n"
+            ,   gnx::ansi::escape[fg::bright_yellow]
+            ,   min_length
+            ,   max_length
+            ,   gnx::ansi::escape[fg::reset]
+            );
+        else
+            fmt::format_to
+            (   std::back_inserter(buf)
+            ,   "└── Read length: {}{}{} bp\n"
+            ,   gnx::ansi::escape[fg::bright_yellow]
+            ,   min_length
+            ,   gnx::ansi::escape[fg::reset]
+            );
+        std::cout.write(buf.data(), buf.size());
+    }
 }
 
 }   // namespace gnx
+
