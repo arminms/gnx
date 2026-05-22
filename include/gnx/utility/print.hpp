f83ffa5449dad55516a1fc6831a391cee71b26f4
Armin Sobhani <arminms@gmail.com>
2026-05-22 19:10:31 -0400

    feat: add print utility functions for formatted output with customizable color schemes

---
+++ include/gnx/utility/print.hpp
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2026 Armin Sobhani
+//
+#pragma once
+
+#include <gnx/utility/detail/print.hpp>
+
+namespace gnx {
+
+template<std::ranges::input_range Range>
+inline std::string print
+(   Range const& range
+,   std::array<std::string, 256> const& color_scheme
+,   std::size_t line_width = 80
+,   std::size_t start_index = 1
+,   std::size_t separator = 10
+)
+{   return detail::print_to_string
+    (   range
+    ,   color_scheme
+    ,   line_width
+    ,   start_index
+    ,   separator
+    );
+}
+
+template<std::ranges::input_range Range>
+inline std::string print
+(   Range const& range
+,   std::size_t line_width = 80
+,   std::size_t start_index = 1
+,   std::size_t separator = 10
+)
+{   return detail::print_to_string
+    (   range
+    ,   gnx::is_peptide(range)
+        ?   gnx::color_scheme::na_warn
+        :   gnx::color_scheme::na
+    ,   line_width
+    ,   start_index
+    ,   separator
+    );
+}
+
+}   // namespace gnx
