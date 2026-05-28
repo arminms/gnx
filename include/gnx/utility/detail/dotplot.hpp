1e962df8935f3c3dc697dacc74acaf3e37d9b218
Armin Sobhani <arminms@gmail.com>
2026-05-28 19:56:14 -0400

    feat: add overloaded dotplot function for multiple range inputs

--- include/gnx/utility/detail/dotplot.hpp
+++ include/gnx/utility/detail/dotplot.hpp
@@ -59,4 +59,67 @@ inline void dotplot_pm3d
     gp.end();
 }
 
+template
+<   std::ranges::random_access_range Range1
+,   std::ranges::random_access_range Range2
+>
+inline void dotplot_image
+(   g3p::gnuplot const& gp
+,   Range1 const& range1
+,   Range2 const& range2
+)
+{   gp
+    (   "unset key; unset colorbox" )
+    (   "set origin 0,0" )
+    (   "set size ratio %f", float(std::size(range2)) / float(std::size(range1)) )
+    (   "set tics out nomirror" )
+    (   "set border 31 linecolor '#555555'" )
+    (   "set xrange [0:%d]", std::size(range1) )
+    (   "set yrange [0:%d]", std::size(range2) )
+    (   "set palette gray" )
+    (   "plot '-' u 1:2:3 w image" )
+    ;
+    for (size_t i = 0; i < std::size(range1); ++i)
+    {   for (size_t j = 0; j < std::size(range2); ++j)
+        {   int c = ((range1[i] ^ range2[j]) & 0xFF) ? 0xFFFFFF : 0x000000;
+            gp << i << j << c << "\n";
+        }
+    }
+    gp.end();
+}
+
+template
+<   std::ranges::random_access_range Range1
+,   std::ranges::random_access_range Range2
+>
+inline void dotplot_pm3d
+(   g3p::gnuplot const& gp
+,   Range1 const& range1
+,   Range2 const& range2
+)
+{   // adjust the tile size based on the sequence length
+    size_t tile_width = static_cast<size_t>(std::log(std::size(range1)));
+    size_t tile_height = static_cast<size_t>(std::log(std::size(range2)));
+    gp
+    (   "set pm3d at b" )
+    (   "unset key; unset colorbox" )
+    (   "set tics out nomirror" )
+    (   "set border 31 linecolor '#555555'" )
+    (   "set view map" )
+    (   "set palette gray" )
+    (   "set size ratio %f", float(std::size(range2)) / float(std::size(range1)) )
+    (   "set ytics add ('' 0)" )
+    (   "set xrange [0:%d]", std::size(range1) )
+    (   "set yrange [0:%d]", std::size(range2) )
+    ;
+    gp
+    (   "splot '-' using 1:2:3 with pm3d" );
+    for (size_t i = 0; i < std::size(range1); i += tile_width)
+    {   for (size_t j = 0; j < std::size(range2); j += tile_height)
+            gp << i << j << (((range1[i] ^ range2[j]) & 0xFF) ? 1 : 0) << "\n";
+        gp << "\n";
+    }
+    gp.end();
+}
+
 } // namespace gnx::detail
--- include/gnx/utility/dotplot.hpp
+++ include/gnx/utility/dotplot.hpp
@@ -53,4 +53,49 @@ inline void dotplot
 #endif // __CLING__
 }
 
+template
+<   std::ranges::random_access_range Range1
+,   std::ranges::random_access_range Range2
+>
+inline void dotplot
+(   Range1 const& range1
+,   Range2 const& range2
+,   std::size_t width = 0
+,   std::string_view filename = ""
+)
+{   std::string term = "pngcairo";
+    if (!filename.empty())
+    {   auto ext = std::filesystem::path(filename).extension().string();
+        if (ext == ".svg")
+            term = "svg";
+        else if (ext == ".pdf")
+            term = "pdfcairo";
+        else if (ext == ".png")
+            term = "pngcairo";
+        else if (ext == ".jpg" || ext == ".jpeg")
+            term = "jpeg";
+        else
+            log_error("unsupported image file format for dotplot output");
+    }
+
+    width = std::max(width, std::size(range1));
+    // cap the width to HD resolution to avoid excessive memory usage
+    width = std::min(width, static_cast<std::size_t>(1920));
+    size_t height = std::size(range2) * width / std::size(range1);
+
+    g3p::gnuplot gp;
+    if (!filename.empty())
+        gp("set output '%s'", filename.data());
+    gp("set term %s size %d,%d", term.c_str(), width, height);
+    std::size(range1) <= 500
+    ?   detail::dotplot_image(gp, range1, range2)
+    :   detail::dotplot_pm3d(gp, range1, range2)
+    ;
+
+#if defined(__CLING__)
+    if (filename.empty())
+        display(gp, false);
+#endif // __CLING__
+}
+
 } // namespace gnx
