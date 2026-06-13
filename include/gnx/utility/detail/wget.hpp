f584143726591e5cdaaefff215b441e3f732921f
Armin Sobhani <arminms@gmail.com>
2026-06-13 15:44:27 -0400

    refactor: update wget_result structure and enhance URL construction functions

--- include/gnx/utility/detail/wget.hpp
+++ include/gnx/utility/detail/wget.hpp
@@ -12,7 +12,8 @@
 #include <filesystem>
 #include <stdexcept>
 
-namespace gnx {
+namespace gnx::detail
+{
 
 struct wget_result
 {   wget_result() = default;
@@ -21,15 +22,109 @@ struct wget_result
     wget_result(wget_result&&) = default;
     wget_result& operator=(wget_result&&) = default;
     wget_result(std::filesystem::path const& temp_file_path)
-    :   temp_file_path(temp_file_path)
+    :   _temp_file_path(temp_file_path)
+    ,   _file_path(temp_file_path.string())
     {}
     ~wget_result()
-    {   if (std::filesystem::exists(temp_file_path))
-            std::filesystem::remove(temp_file_path);
+    {   if (std::filesystem::exists(_temp_file_path))
+            std::filesystem::remove(_temp_file_path);
+    }
+    std::string_view operator() () const noexcept
+    {   return _file_path;
+    }
+    std::filesystem::path file_path() const noexcept
+    {   return _temp_file_path;
+    }
+    void close() noexcept
+    {   if (std::filesystem::exists(_temp_file_path))
+            std::filesystem::remove(_temp_file_path);
+        _file_path.clear();
     }
 
-// private:
-    std::filesystem::path temp_file_path;
+private:
+    std::filesystem::path _temp_file_path;
+    std::string _file_path;
 };
 
-} // namespace gnx
+[[nodiscard]]
+inline bool is_valid_url(std::string_view url)
+{   return
+    (   url.starts_with("ftp://")
+    ||  url.starts_with("http://")
+    ||  url.starts_with("https://")
+    ||  url.starts_with("genome://")
+    ||  url.starts_with("sra://")
+    );  
+}
+
+[[nodiscard]]
+inline std::string construct_genome_url(std::string_view genome_acn_n_assembly)
+{   genome_acn_n_assembly.remove_prefix(9); // remove "genome://"
+    return fmt::format
+    (   "ftp://ftp.ncbi.nlm.nih.gov/genomes/all/{}/{}/{}/{}/{}/{}_genomic.fna.gz"
+    ,   genome_acn_n_assembly.substr(0, 3)
+    ,   genome_acn_n_assembly.substr(4, 3)
+    ,   genome_acn_n_assembly.substr(7, 3)
+    ,   genome_acn_n_assembly.substr(10, 3)
+    ,   genome_acn_n_assembly
+    ,   genome_acn_n_assembly
+    );
+}
+
+[[nodiscard]]
+inline std::string construct_sra_url(std::string_view run_acn)
+{   std::string result, base{"ftp://ftp.sra.ebi.ac.uk/vol1/fastq"};
+    run_acn.remove_prefix(6); // remove "sra://"
+    auto pos = run_acn.rfind('_');
+    auto accession
+    =   pos == std::string_view::npos
+    ?   std::string(run_acn)
+    :   std::string(run_acn.substr(0, pos));
+    if (accession.size() == 9)
+        result = fmt::format
+        (   "{}/{}/{}/{}.fastq.gz"
+        ,   base
+        ,   accession.substr(0, 6)
+        ,   accession
+        ,   run_acn
+        );
+    else if (accession.size() == 10)
+        result = fmt::format
+        (   "{}/{}/{:0>3}/{}/{}.fastq.gz"
+        ,   base
+        ,   accession.substr(0, 6)
+        ,   accession.substr(9, 1)
+        ,   accession
+        ,   run_acn
+        );
+    else if (accession.size() == 11)
+        result = fmt::format
+        (   "{}/{}/{:0>3}/{}/{}.fastq.gz"
+        ,   base
+        ,   accession.substr(0, 6)
+        ,   accession.substr(9, 2)
+        ,   accession
+        ,   run_acn
+        );
+    else if (accession.size() >= 12)
+        result = fmt::format
+        (   "{}/{}/{}/{}/{}.fastq.gz"
+        ,   base
+        ,   accession.substr(0, 6)
+        ,   accession.substr(9, 3)
+        ,   accession
+        ,   run_acn
+        );
+    else
+        throw std::invalid_argument
+        (   fmt::format
+            (   "Invalid SRA accession in '{}'. Expected format: "
+                "'sra://SRR12345678' or 'sra://SRR12345678_1'"
+            ,   run_acn
+            )
+        );
+    
+    return result;
+}
+
+} // namespace gnx::detail
